2b7fe69f415fbffad6d831b651eb2d3a09834b77
Dmitry Petrov <dimaninc@gmail.com>
2024-09-11 10:05:40 +0300

    updated admin's index.php to catch exceptions on start

--- htdocs/_admin/index.php
+++ htdocs/_admin/index.php
@@ -1,8 +1,21 @@
 <?php
+
+use diCore\Data\Http\HttpCode;
+use diCore\Tool\Logger;
+use NewProject\Admin\Base;
+use NewProject\Base\CMS;
+
 error_reporting(E_ALL);
 
-require '../../vendor/dimaninc/di_core/php/functions.php';
-require '../../_cfg/common.php';
+try {
+    require '../../vendor/dimaninc/di_core/php/functions.php';
+    require '../../_cfg/common.php';
 
-$X = new \NewProject\Admin\Base();
-$X->work();
+    $X = new Base();
+    $X->work();
+} catch (Exception $e) {
+    HttpCode::header(HttpCode::INTERNAL_SERVER_ERROR);
+    echo '<h1>Error</h1>';
+    echo CMS::isProd() ? 'See logs for details' : $e->getMessage();
+    Logger::getInstance()->variable($e);
+}
